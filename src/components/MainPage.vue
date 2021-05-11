f1a7bd88c3b3ddca882210130496ec5f1b7cf3a3
Peter <griffitp12@gmail.com>
2021-05-11 18:06:04 +0900

    updated Mentor data

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main-page">
-    <Mentor :mentorData="this.mentors[0]"/>
+    <div class="mentor-one"><Mentor :mentorData="this.mentors[0]" /></div>
+    <div class="empty-div"></div>
   </div>
 </template>
 
@@ -15,7 +16,12 @@ export default {
   data: function () {
     return {
       mentors: [
-        { name: "Ken", photoURL: "https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y", bio: "Sgt Corporal in the CC Army of Awesome" },
+        {
+          name: "Ken",
+          photoURL:
+            "https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y",
+          bio: "Sgt Corporal in the CC Army of Awesome",
+        },
         { name: "Johan", photoURL: "" },
         { name: "Mussin", photoURL: "" },
       ],
@@ -27,8 +33,19 @@ export default {
 <style>
 .main-page {
   background-color: hotpink;
-  border: 1px dotted hotpink;
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.mentor-one {
+  /* border: 3px solid hotpink; */
+  flex: 2
+}
+
+.empty-div {
+  /* border: 1px solid red; */
+  flex: 1
 }
 </style>
--- src/components/Mentor.vue
+++ src/components/Mentor.vue
@@ -3,17 +3,17 @@
     <b-card
       :title="this.mentorData.name"
       :img-src="this.mentorData.photoURL"
-      img-alt="Image"
+      :img-alt="this.mentorData.name"
       img-top
       tag="article"
-      style="max-width: 20rem"
+      style="max-width: 15rem"
       class="mb-2"
     >
       <b-card-text>
         {{this.mentorData.bio}}
       </b-card-text>
 
-      <b-button href="#" variant="primary">Go somewhere</b-button>
+      <b-button href="#" variant="primary">Book Me</b-button>
     </b-card>
   </div>
 </template>
