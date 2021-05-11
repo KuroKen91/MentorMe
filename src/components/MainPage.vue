2b2214c219633260ce7541e8881590c695969156
Peter <griffitp12@gmail.com>
2021-05-11 19:15:25 +0900

    Added additional mentors

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="main-page">
-    <div class="mentor-one"><Mentor :mentorData="this.mentors[0]" /></div>
-    <div class="empty-div"></div>
+    <div class="mentor-one mentor">
+      <Mentor :mentorData="this.mentors[0]" />
+    </div>
+    <div class="mentor-two mentor">
+      <Mentor :mentorData="this.mentors[1]" />
+    </div>
+    <div class="mentor-three mentor">
+      <Mentor :mentorData="this.mentors[2]" />
+    </div>
+    <!-- <div class="empty-div"></div> -->
   </div>
 </template>
 
@@ -22,8 +30,18 @@ export default {
             "https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y",
           bio: "Sgt Corporal in the CC Army of Awesome",
         },
-        { name: "Johan", photoURL: "" },
-        { name: "Mussin", photoURL: "" },
+        {
+          name: "Johan",
+          photoURL:
+            "https://i.picsum.photos/id/1024/1920/1280.jpg?hmac=-PIpG7j_fRwN8Qtfnsc3M8-kC3yb0XYOBfVzlPSuVII",
+          bio: "Björn Borg Reborn",
+        },
+        {
+          name: "Mussin",
+          photoURL:
+            "https://i.picsum.photos/id/1074/5472/3648.jpg?hmac=w-Fbv9bl0KpEUgZugbsiGk3Y2-LGAuiLZOYsRk0zo4A",
+          bio: "Recursively Functional ",
+        },
       ],
     };
   },
@@ -39,13 +57,12 @@ export default {
   justify-content: center;
   align-items: center;
 }
-.mentor-one {
-  /* border: 3px solid hotpink; */
-  flex: 2
+.mentor {
+  margin: 10px;
 }
 
 .empty-div {
   /* border: 1px solid red; */
-  flex: 1
+  flex: 1;
 }
 </style>
