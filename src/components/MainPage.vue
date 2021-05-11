d5dbd6f05f60898ffb9ff4292339ad21cc183249
Peter <griffitp12@gmail.com>
2021-05-11 17:49:19 +0900

    set up Mentor Ken

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -15,7 +15,7 @@ export default {
   data: function () {
     return {
       mentors: [
-        { name: "Ken", photoURL: "https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y" },
+        { name: "Ken", photoURL: "https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y", bio: "Sgt Corporal in the CC Army of Awesome" },
         { name: "Johan", photoURL: "" },
         { name: "Mussin", photoURL: "" },
       ],
--- src/components/Mentor.vue
+++ src/components/Mentor.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-card
-      title="Card Title"
+      :title="this.mentorData.name"
       :img-src="this.mentorData.photoURL"
       img-alt="Image"
       img-top
@@ -10,8 +10,7 @@
       class="mb-2"
     >
       <b-card-text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+        {{this.mentorData.bio}}
       </b-card-text>
 
       <b-button href="#" variant="primary">Go somewhere</b-button>
@@ -25,7 +24,8 @@ export default {
   props: {
       mentorData: {
           name: String,
-          photoURL: String
+          photoURL: String,
+          bio: String,
       }
   }
 };
