80620cdf8ab1784c070783e1bdcedd4aeb20fc78
Peter <griffitp12@gmail.com>
2021-05-11 16:15:46 +0900

    installed bootstrap

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,17 @@
 <script>
 import Splash from './components/Splash.vue'
 import MainPage from './components/MainPage.vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 export default {
   name: 'App',
