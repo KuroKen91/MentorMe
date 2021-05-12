71e0849bd2e4d3d4bf08ac3907fcdede58236c47
Peter <griffitp12@gmail.com>
2021-05-12 16:54:30 +0900

    added BASIC success page

--- src/components/Successmodal.vue
+++ src/components/Successmodal.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+    <!-- <b-button v-b-modal.modal-1>Launch demo modal</b-button>
 
     <b-modal id="modal-1" title="BootstrapVue">
       <p class="my-4">Hello from modal!</p>
-    </b-modal>
+    </b-modal> -->
+    <h3>This isn't a modal yet, but it is a success page!</h3>
   </div>
 </template>
 
