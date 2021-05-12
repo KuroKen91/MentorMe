217b537b61e208e769f4e7b471320bf60604434f
Peter <griffitp12@gmail.com>
2021-05-12 19:29:15 +0900

    resetting success

---
+++ src/views/Success.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="success">
+    <h1>Thanks for shopping...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Successmodal",
+};
+</script>
+
+<style>
+.success {
+  background-color: #b4d455;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
